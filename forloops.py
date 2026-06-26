d4c66e41303134aa9cd06de2cb00be946d4bebee
MrQamar <shaikqamar.rocks@gmail.com>
2026-06-26 19:01:18 +0530

    Updated loops again and coded hollow patterns

--- forloops.py
+++ forloops.py
@@ -9,7 +9,15 @@ a = int(input("Enter a number to create a star pattern right angled Triangle: ")
 # for i in range((a - 1), 0, -1):
 #     print((" " * (a - i)) + "* "* i)
 
-for i in range(1, a + 1):
-    print("* " * i + "  " * ((a - i) * 2) + "* " * i)
-for i in range(a - 1, 0 , -1):
-    print("* " * i + "  " * ((a - i) * 2) + "* " * i)
+# for i in range(1, a + 1):
+#     print("* " * i + "  " * ((a - i) * 2) + "* " * i)
+# for i in range(a - 1, 0 , -1):
+#     print("* " * i + "  " * ((a - i) * 2) + "* " * i)
+print(" " * (a - 1) +"*")
+
+for i in range(2, a):
+    hollowL = " " * (a - i)
+    hollowC = "  " * (i - 2) 
+    print(hollowL + "* " + hollowC + "*")
+
+print("* " * a)
